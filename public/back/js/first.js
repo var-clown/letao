c08dc7fb55d1535df5df62f6103189f096d00e3d
var-clown <2268844683@example.co>
2019-02-24 22:03:43 +0800

    一级分类完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -1,6 +1,6 @@
 $(function () {
     var currenPage = 1;
-    var pageSize = 3;
+    var pageSize = 5;
     render();
 
     function render() {
@@ -38,6 +38,48 @@ $(function () {
     // 2.点击添加分类按钮，弹出模态框
     $(".addBtn").on("click",function(){
         $("#addCategory").modal('show');
+    })
+
+    // 3.表单的校验功能
+    $("#form-add").bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+          },
+
+        fields:{
+            categoryName:{
+                validators:{
+                    notEmpty:{
+                        message: '用户名不能为空'
+                    }
+                }
+            }
+        }
+    })
+
+    // 4.注册表单校验成功事件，取消默认发送方式，用ajax方式发送请求
+    $('#form-add').on('success.form.bv', function( e ){
+        e.preventDefault(); // 阻止默认的提交
+
+        $.ajax({
+            type:"post",
+            url:"/category/addTopCategory",
+            data:$("#form-add").serialize(),
+            dataType:"json",
+            success:function(info){
+                // console.log(info);
+                // 隐藏模态框
+                $("#addCategory").modal('hide');
+                // 重新渲染表单
+                render();
+
+                //重置表单
+                $("#form-add").data('bootstrapValidator').resetForm(true)
+                
+            }   
+        })
     })
 
 })
